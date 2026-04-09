2d4e1d307c1d17a6eed7d862fc3826bef345d36a
LendyAbel <lendyabel93@gmail.com>
2026-04-09 17:16:04 +0200

    [work] implementing users and auth

---
+++ backend/src/modules/auth/schemas/authZodSchema.ts
@@ -0,0 +1,15 @@
+import z from 'zod';
+
+export const RegisterSchema = z.object({
+    name: z.string().min(1, 'Name is required').trim(),
+    email: z.email('Invalid email'),
+    password: z.string().min(1, 'Password is required'),
+});
+
+export const LoginSchema = z.object({
+    email: z.email('Invalid email'),
+    password: z.string().min(1, 'Password is required'),
+});
+
+export type RegisterInput = z.infer<typeof RegisterSchema>;
+export type LoginInput = z.infer<typeof LoginSchema>;
---
+++ backend/src/modules/auth/services/authServices.ts
@@ -0,0 +1,49 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { LoginInput, RegisterInput } from '../schemas/authZodSchema';
+import { prisma } from '../../../lib/prisma';
+import { AppError } from '../../../lib/AppError';
+
+const SALT_ROUNDS = 10;
+
+const register = async (data: RegisterInput) => {
+    const hashedPassword = await bcrypt.hash(data.password, SALT_ROUNDS);
+
+    const user = await prisma.user.create({
+        data: {
+            name: data.name,
+            email: data.email,
+            password: hashedPassword,
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            createdAt: true,
+        },
+    });
+    return user;
+};
+
+const login = async (data: LoginInput) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: data.email,
+        },
+    });
+    if (!user) throw new AppError('Invalid credentials', 401);
+
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
+    if (!isPasswordValid) throw new AppError('Invalid credentials', 401);
+
+    const token = jwt.sign(
+        { userId: user.id, role: user.role },
+        process.env.JWT_SECRET!,
+        { expiresIn: '7d' },
+    );
+    const { password: _, ...userWithoutPassword } = user;
+    return { token, user: userWithoutPassword };
+};
+
+export default { register, login };
